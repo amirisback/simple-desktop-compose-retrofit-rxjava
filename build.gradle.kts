9abff93cab6aaaf779100a71332693053a9d9f23
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-10-09 14:38:59 +0700

    add: Dependencies Retrofit And RxJava

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,24 @@ repositories {
 
 dependencies {
     implementation(compose.desktop.currentOs)
+
+    implementation("com.google.code.gson:gson:2.8.8")
+
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+
+    implementation("io.insert-koin:koin-core:3.1.2")
+    implementation("io.reactivex.rxjava2:rxjava:2.2.21")
+
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
+
+    testImplementation("io.insert-koin:koin-test:3.1.2")
+
 }
 
 tasks.withType<KotlinCompile>() {
